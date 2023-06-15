84e0dff5ef71960da8df8071ab3f94f38f4e68f5
Ido alon <ido.alon@post.runi.ac.il>
2023-06-15 17:49:07 +0300

    almost finish

--- src/hw5.js
+++ src/hw5.js
@@ -61,149 +61,158 @@ function scale_matrix(x, y, z){
 // Create the goal gate
 const goal = new THREE.Group();
 
+// from this number we will fit the whole scene
+let crossbar_length = 3
+
+// crossbar width
+let crossbar_width = crossbar_length / 150
+
+// crossbar height from origin
+let height_from_origin = crossbar_length / 9
+
+// angle for the back support rotations
+let angle = Math.PI / 4
+
 // create crossbar
-const cross_bar_geometry = new THREE.CylinderGeometry(0.06, 0.06, 9);
-const cross_bar_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const cross_bar_geometry = new THREE.CylinderGeometry(crossbar_width, crossbar_width, crossbar_length);
+const cross_bar_material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const cross_bar = new THREE.Mesh(cross_bar_geometry, cross_bar_material);
 const m_rotation_cross = rotation_matrix('z', Math.PI / 2);
 cross_bar.applyMatrix4(m_rotation_cross);
-const m_trans_cross = translate_matrix(0, 1, 0 );
+const m_trans_cross = translate_matrix(0, height_from_origin, 0 );
 cross_bar.applyMatrix4(m_trans_cross);
 
 // add crossbar to goal
 goal.add(cross_bar);
 
 // create right goal post
-const right_post_geometry = new THREE.CylinderGeometry(0.06, 0.06, 3);
-const right_post_geometry_meterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const right_post_geometry = new THREE.CylinderGeometry(crossbar_width, crossbar_width, crossbar_length / 3);
+const right_post_geometry_meterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const right_post = new THREE.Mesh(right_post_geometry, right_post_geometry_meterial);
-const m_trans_post_right = translate_matrix(4.5, -0.5, 0)
+const m_trans_post_right = translate_matrix(crossbar_length / 2, -height_from_origin / 2, 0)
 right_post.applyMatrix4(m_trans_post_right)
 
 // create torus for the edge of the right goal post
-const right_post_torus_geometry = new THREE.TorusGeometry(0.08, 0.04, 8, 16);
-const right_post_torus_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const right_post_torus_geometry = new THREE.TorusGeometry(crossbar_width + 0.02, crossbar_width - 0.02, (crossbar_length + 7) / 2, crossbar_length + 7);
+const right_post_torus_material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const right_post_torus = new THREE.Mesh(right_post_torus_geometry, right_post_torus_material);
 const m_rotation_torus_right = rotation_matrix('x', Math.PI / 2);
 right_post_torus.applyMatrix4(m_rotation_torus_right);
-const m_trans_torus_right = translate_matrix(4.5, -2, 0)
+const m_trans_torus_right = translate_matrix(crossbar_length / 2, -height_from_origin * 2, 0)
 right_post_torus.applyMatrix4(m_trans_torus_right)
 
 // create left goal post
-const left_post_geometry = new THREE.CylinderGeometry(0.06, 0.06, 3);
-const left_post_geometry_meterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const left_post_geometry = new THREE.CylinderGeometry(crossbar_width, crossbar_width, crossbar_length / 3);
+const left_post_geometry_meterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const left_post = new THREE.Mesh(left_post_geometry, left_post_geometry_meterial);
-const m_trans_post_left = translate_matrix(-4.5, -0.5, 0)
+const m_trans_post_left = translate_matrix(-crossbar_length / 2, -height_from_origin / 2, 0)
 left_post.applyMatrix4(m_trans_post_left)
 
 // create torus for the edge of the left goal post
-const left_post_torus_geometry = new THREE.TorusGeometry(0.08, 0.04, 8, 16);
-const left_post_torus_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const left_post_torus_geometry = new THREE.TorusGeometry(crossbar_width + 0.02, crossbar_width - 0.02, (crossbar_length + 7) / 2,  crossbar_length + 7);
+const left_post_torus_material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const left_post_torus = new THREE.Mesh(left_post_torus_geometry, left_post_torus_material);
 const m_rotation_torus_left = rotation_matrix('x', Math.PI / 2);
 left_post_torus.applyMatrix4(m_rotation_torus_left);
-const m_trans_torus_left = translate_matrix(-4.5, -2, 0)
+const m_trans_torus_left = translate_matrix(-crossbar_length / 2, -height_from_origin * 2, 0)
 left_post_torus.applyMatrix4(m_trans_torus_left)
 
 // add posts to goal
 goal.add(right_post,right_post_torus,left_post_torus, left_post);
 
 // create back right support
-const back_right_support_geometry = new THREE.CylinderGeometry(0.06, 0.06, 4.2);
-const back_right_support_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+let back_support_length = (crossbar_length / 3) / Math.cos(angle)
+const back_right_support_geometry = new THREE.CylinderGeometry(crossbar_width, crossbar_width, back_support_length);
+const back_right_support_material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const back_right_support = new THREE.Mesh(back_right_support_geometry, back_right_support_material);
 const m_back_right_rotation = rotation_matrix('x', Math.PI / 4);
 back_right_support.applyMatrix4(m_back_right_rotation);
-const m_back_right_trans = translate_matrix(4.5, -0.5, -1.5);
+const m_back_right_trans = translate_matrix(crossbar_length / 2, -height_from_origin / 2, -crossbar_length / 6);
 back_right_support.applyMatrix4(m_back_right_trans)
 
 // create torus for the edge of the back_right_support
-const right_back_torus_geometry = new THREE.TorusGeometry(0.08, 0.04, 8, 16);
-const right_back_torus_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const right_back_torus_geometry = new THREE.TorusGeometry(crossbar_width + 0.02, crossbar_width - 0.02, (crossbar_length + 7) / 2, crossbar_length + 7);
+const right_back_torus_material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const right_back_torus = new THREE.Mesh(right_back_torus_geometry, right_back_torus_material);
 const m_rotation_torus_back_right = rotation_matrix('x', Math.PI / 2);
 right_back_torus.applyMatrix4(m_rotation_torus_back_right);
-const m_trans_torus_back_right = translate_matrix(4.5, -2, -3);
+const m_trans_torus_back_right = translate_matrix(crossbar_length / 2, -height_from_origin * 2, -crossbar_length / 3);
 right_back_torus.applyMatrix4(m_trans_torus_back_right);
 
 // create back left support
-const back_left_support_geometry = new THREE.CylinderGeometry(0.06, 0.06, 4.2);
-const back_left_support_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const back_left_support_geometry = new THREE.CylinderGeometry(crossbar_width, crossbar_width, back_support_length);
+const back_left_support_material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const back_left_support = new THREE.Mesh(back_left_support_geometry, back_left_support_material);
 const m_back_left_rotation = rotation_matrix('x', Math.PI / 4);
 back_left_support.applyMatrix4(m_back_left_rotation);
-const m_back_left_trans = translate_matrix(-4.5, -0.5, -1.5);
+const m_back_left_trans = translate_matrix(-crossbar_length / 2, -height_from_origin / 2, -crossbar_length / 6);
 back_left_support.applyMatrix4(m_back_left_trans)
 
 // create torus for the edge of the back_right_support
-const left_back_torus_geometry = new THREE.TorusGeometry(0.08, 0.04, 8, 16);
-const left_back_torus_material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const left_back_torus_geometry = new THREE.TorusGeometry(crossbar_width + 0.02, crossbar_width - 0.02, (crossbar_length + 7) / 2, crossbar_length + 7);
+const left_back_torus_material = new THREE.MeshPhongMaterial({ color: 0xffffff });
 const left_back_torus = new THREE.Mesh(left_back_torus_geometry, left_back_torus_material);
 const m_rotation_torus_back_left = rotation_matrix('x', Math.PI / 2);
 left_back_torus.applyMatrix4(m_rotation_torus_back_left);
-const m_trans_torus_back_left = translate_matrix(-4.5, -2, -3);
+const m_trans_torus_back_left = translate_matrix(-crossbar_length / 2, -height_from_origin * 2, -crossbar_length / 3);
 left_back_torus.applyMatrix4(m_trans_torus_back_left);
 
 // add back support
 goal.add(back_right_support, right_back_torus, back_left_support, left_back_torus)
 
 // create back net
-const back_net_Geometry = new THREE.PlaneGeometry(9, 4.2);
-const back_net_Material = new THREE.MeshBasicMaterial({ color: 0xD3D3D3, side: THREE.DoubleSide});
+const back_net_Geometry = new THREE.PlaneGeometry(crossbar_length , back_support_length);
+const back_net_Material = new THREE.MeshPhongMaterial({ color: 0xD3D3D3, side: THREE.DoubleSide});
 const back_net = new THREE.Mesh(back_net_Geometry, back_net_Material);
 const m_back_Net_rotation = rotation_matrix('x', Math.PI / 4)
 back_net.applyMatrix4(m_back_Net_rotation)
-const m_back_Net_trans = translate_matrix(0, -0.5, -1.5)
+const m_back_Net_trans = translate_matrix(0, -height_from_origin / 2, -crossbar_length / 6)
 back_net.applyMatrix4(m_back_Net_trans)
 
 // create right side net
-const right_triangle_geometry = new THREE.BufferGeometry();
-const right_triangle_vertices = new Float32Array([
-	1, 1, 0,  // Vertex 1
-	-2, -2, 0, // Vertex 2
-	1, -2, 0   // Vertex 3
-]);
-right_triangle_geometry.setAttribute('position', new THREE.BufferAttribute(right_triangle_vertices, 3));
-const right_material = new THREE.MeshBasicMaterial({ color: 0xD3D3D3 ,side: THREE.DoubleSide});
+const right_triangle_Shape = new THREE.Shape();
+right_triangle_Shape.lineTo(-height_from_origin * 2, -height_from_origin * 2); // Vertex 2
+right_triangle_Shape.lineTo(crossbar_length / 9, -height_from_origin * 2); // Vertex 3
+right_triangle_Shape.lineTo(crossbar_length / 9, crossbar_length / 9); // Vertex 1
+const right_triangle_geometry = new THREE.ShapeGeometry(right_triangle_Shape);
+const right_material = new THREE.MeshPhongMaterial({ color: 0xD3D3D3, side: THREE.DoubleSide });
 const right_triangle = new THREE.Mesh(right_triangle_geometry, right_material);
 const m_rotation_triangle_right = rotation_matrix('y', -Math.PI / 2);
 right_triangle.applyMatrix4(m_rotation_triangle_right);
-const m_trans_triangle_right = translate_matrix(4.5, 0, -1);
+const m_trans_triangle_right = translate_matrix(crossbar_length / 2, 0, -height_from_origin);
 right_triangle.applyMatrix4(m_trans_triangle_right);
 
 // create left side net
-const left_triangle_geometry = new THREE.BufferGeometry();
-const left_triangle_vertices = new Float32Array([
-	1, 1, 0,  // Vertex 1
-	-2, -2, 0, // Vertex 2
-	1, -2, 0   // Vertex 3
-]);
-left_triangle_geometry.setAttribute('position', new THREE.BufferAttribute(left_triangle_vertices, 3));
-const left_material = new THREE.MeshBasicMaterial({ color: 0xD3D3D3 ,side: THREE.DoubleSide,});
+const left_triangle_Shape = new THREE.Shape();
+left_triangle_Shape.lineTo(-height_from_origin * 2, -height_from_origin * 2); // Vertex 2
+left_triangle_Shape.lineTo(crossbar_length / 9, -height_from_origin * 2); // Vertex 3
+left_triangle_Shape.lineTo(crossbar_length / 9, crossbar_length / 9); // Vertex 1
+const left_triangle_geometry = new THREE.ShapeGeometry(left_triangle_Shape);
+const left_material = new THREE.MeshPhongMaterial({ color: 0xD3D3D3, side: THREE.DoubleSide });
 const left_triangle = new THREE.Mesh(left_triangle_geometry, left_material);
 const m_rotation_triangle_left = rotation_matrix('y', -Math.PI / 2);
 left_triangle.applyMatrix4(m_rotation_triangle_left);
-const m_trans_triangle_left = translate_matrix(-4.5, 0, -1);
+const m_trans_triangle_left = translate_matrix(-crossbar_length / 2, 0, -height_from_origin);
 left_triangle.applyMatrix4(m_trans_triangle_left);
 
 goal.add(back_net, right_triangle, left_triangle);
 
 // create ball
-const ballGeometry = new THREE.SphereGeometry(3/16, 32, 32);
-const ballMaterial = new THREE.MeshBasicMaterial({
+const ballGeometry = new THREE.SphereGeometry(((crossbar_length / 3) / 8) / 2, crossbar_length * 4, crossbar_length * 4);
+const ballMaterial = new THREE.MeshPhongMaterial({
 	color: 0x000000, wireframe: false,});
 const ball = new THREE.Mesh( ballGeometry, ballMaterial );
-const m_ball_trans = translate_matrix(0, -2, 2)
+const m_ball_trans = translate_matrix(0, -height_from_origin * 2 + 0.1, height_from_origin * 2)
 ball.applyMatrix4(m_ball_trans)
 
 const loader = new THREE.ImageLoader();
 loader.load('goalkeeper.png', function (image) {
 		const texture = new THREE.Texture(image);
 		texture.needsUpdate = true;
-		const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true, alpha: 0.5, side : THREE.DoubleSide });
-		const geometry = new THREE.PlaneGeometry(5, 3);
+		const material = new THREE.MeshPhongMaterial({ map: texture, transparent: true, alpha: 0.5, side : THREE.DoubleSide });
+		const geometry = new THREE.PlaneGeometry(crossbar_length / 1.8, crossbar_length / 3);
 		const mesh = new THREE.Mesh(geometry, material);
-		const m_goalkeeper_trans = translate_matrix(0, -1, 0.5)
+		const m_goalkeeper_trans = translate_matrix(0, -height_from_origin, height_from_origin / 2)
 		mesh.applyMatrix4(m_goalkeeper_trans)
 		mesh.name = "goalkeeper"
 		scene.add(mesh);
@@ -218,6 +227,13 @@ loader.load('goalkeeper.png', function (image) {
 scene.add(goal)
 scene.add(ball);
 
+//add spotlight source
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+const spotLight = new THREE.SpotLight(0xffffff);
+spotLight.intensity = 0;
+scene.add(spotLight);
+
 // Load the font file
 const fontLoader = new THREE.FontLoader();
 fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
@@ -226,7 +242,7 @@ fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.
 		'\nup/down - speed ball\n1/2 - trajectory ball\n3 - shrink goal' +
 		'\nright/left - move goalkeeper', {
 		font: font,
-		size: 0.17,
+		size: crossbar_width * 3.5,
 		height: 0.1,
 		curveSegments: 0.8,
 		bevelEnabled: true,
@@ -236,14 +252,17 @@ fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.
 	});
 
 	// Create material for the text
-	const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+	const textMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
 
 	// Create mesh using the text geometry and material
 	const textMesh = new THREE.Mesh(textGeometry, textMaterial);
 
-	textMesh.rotation.set(Math.PI / 6, 0, 0);
+
 	// Set the position and rotation of the text
-	textMesh.position.set(-1.5, 3, 0);
+	const m_rotation_text = rotation_matrix('x', Math.PI / 6);
+	textMesh.applyMatrix4(m_rotation_text);
+	const m_trans_text = translate_matrix(-crossbar_length / 6, crossbar_length / 3, 0);
+	textMesh.applyMatrix4(m_trans_text);
 
 	// Add the text mesh to the scene
 	scene.add(textMesh);
@@ -296,15 +315,15 @@ const toggleOrbit = (e) => {
 			speedFactor -= Math.PI / 64;
 		}
 		if(e.key === "ArrowRight") {
-			if(goalkeeper.position.x < 3) {
-				scaleForGoalkeeper += 0.00005;
+			if(goalkeeper.position.x < crossbar_length / 3) {
+				scaleForGoalkeeper += crossbar_length / 10000;
 				const m_goalkeeper_trans = translate_matrix(scaleForGoalkeeper, 0, 0)
 				goalkeeper.applyMatrix4(m_goalkeeper_trans)
 			}
 		}
 		if(e.key === "ArrowLeft") {
-			if(goalkeeper.position.x > -2) {
-				scaleForGoalkeeper -= 0.00005;
+			if(goalkeeper.position.x > -(crossbar_length / 3) + height_from_origin) {
+				scaleForGoalkeeper -= crossbar_length / 10000;
 				const m_goalkeeper_trans = translate_matrix(-scaleForGoalkeeper, 0, 0)
 				goalkeeper.applyMatrix4(m_goalkeeper_trans)
 			}
